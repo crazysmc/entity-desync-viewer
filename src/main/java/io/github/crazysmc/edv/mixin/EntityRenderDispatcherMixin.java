765abb65bf8b46ceae1335553c3476a4e7678147
smc <crazysmc@users.noreply.github.com>
2024-11-18 17:09:57 +0100

    1.21 backport

--- src/main/java/io/github/crazysmc/edv/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/io/github/crazysmc/edv/mixin/EntityRenderDispatcherMixin.java
@@ -1,16 +1,14 @@
 package io.github.crazysmc.edv.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderDispatcher.class)
 public abstract class EntityRenderDispatcherMixin
@@ -20,20 +18,22 @@ public abstract class EntityRenderDispatcherMixin
   {
   }
 
-  @WrapOperation(method = "render(Lnet/minecraft/world/entity/Entity;DDDFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/renderer/entity/EntityRenderer;)V",
-                 at = @At(value = "INVOKE",
-                          target = "Lnet/minecraft/client/renderer/entity/EntityRenderDispatcher;renderHitbox(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/entity/Entity;FFFF)V"))
-  private void wrapRenderHitbox(PoseStack poseStack, VertexConsumer vertexConsumer, Entity entity, float tickDelta,
-                                float r, float g, float b, Operation<Void> original,
-                                @Local(argsOnly = true) MultiBufferSource multiBufferSource)
+  @Inject(method = "renderServerSideHitbox",
+          at = @At(value = "INVOKE",
+                   target = "Lnet/minecraft/client/renderer/debug/DebugRenderer;renderFloatingText(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;Ljava/lang/String;DDDI)V"),
+          cancellable = true)
+  private static void noFloatingText(PoseStack poseStack, Entity entity, MultiBufferSource multiBufferSource,
+                                     CallbackInfo ci)
   {
-    try
-    {
-      renderServerSideHitbox(poseStack, entity, multiBufferSource);
-      original.call(poseStack, vertexConsumer, entity, tickDelta, r, g, b);
-    }
-    catch (IllegalStateException ignored)
-    {
-    }
+    ci.cancel();
+  }
+
+  @Inject(method = "render",
+          at = @At(value = "INVOKE",
+                   target = "Lnet/minecraft/client/renderer/entity/EntityRenderDispatcher;renderHitbox(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/entity/Entity;FFFF)V"))
+  private void renderServerSideHitbox(Entity entity, double d, double e, double f, float g, float h,
+                                      PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci)
+  {
+    renderServerSideHitbox(poseStack, entity, multiBufferSource);
   }
 }
